f400eabbe66f6a78c8f9fb7868de0111d4a96450
SonJY <sjy9428@naver.com>
2023-03-04 17:49:56 +0900

    20230304

---
+++ PRECAMP/class/03-javascript/01-variable.js
@@ -0,0 +1,5 @@
+const name = 123
+
+name = 321
+
+console.log(name)
---
+++ PRECAMP/class/03-javascript/연습-01-array.js
@@ -0,0 +1,17 @@
+let classmates = ["철수", "영희", "훈이"]
+// undefined
+classmates
+// (3) ['철수', '영희', '훈이']
+classmates[1]
+// '영희'
+classmates[0]
+// '철수'
+classmates[2]
+// '훈이'
+classmates.includes("철수")
+// true
+classmates.length
+// 3
+classmates.push("맹구")
+
+  
